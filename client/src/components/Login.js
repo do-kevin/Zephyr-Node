114f8e81a532ebd1eaf03697ebf01825f2abcfb2
Kevin Do <do.kevin.vo@gmail.com>
2018-10-27 23:55:32 -0700

    Adds reactstrap and starting code for Login component

---
+++ client/src/components/Login.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Button color="primary" onClick={this.toggle}>
+          {this.props.buttonLabel}
+          Login
+        </Button>
+        <Modal
+          isOpen={this.state.modal}
+          toggle={this.toggle}
+          className={this.props.className}
+        >
+          <ModalHeader toggle={this.toggle}>Login</ModalHeader>
+          <ModalBody>
+            <form>
+              <div class="form-group">
+                <label for="username">Username</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="username"
+                  placeholder="mysteryperson123"
+                />
+                <label for="password">Password</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="password"
+                />
+              </div>
+            </form>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.toggle}>
+              Sign In
+            </Button>{" "}
+            <Button color="secondary" onClick={this.toggle}>
+              Close
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default Login;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,145 +1,151 @@
 import React from "react";
-import "../css/Home.css";
+
+// Components
 import Search from "../components/Search";
+import Login from "../components/Login";
 
-const Home = () => {
-  return (
-    <div>
-      <nav className="navbar justify-content-end">
-        <button className="btn btn-primary btn-large" type="button">
-          Login
-        </button>
-      </nav>
-      <div className="jumbotron banner-image">
-        <div className="banner-text">
-          <h1>Wholesome and Inspirational Quote</h1>
-          <p>Current date and time or quotist</p>
-          <br />
-          <Search />
+// CSS
+import "../css/Home.css";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar justify-content-end">
+          <Login />
+        </nav>
+        <div className="jumbotron banner-image">
+          <div className="banner-text">
+            <h1>Wholesome and Inspirational Quote</h1>
+            <p>Current date and time or quotist</p>
+            <br />
+            <Search />
+          </div>
+        </div>
+        <h1 className="text-center display-3">Features</h1>
+        <div className="container text-center">
+          <div className="row">
+            <div className="col">
+              <div className="card animated slideInLeft">
+                <div className="card-body feature-cards">
+                  <h1>
+                    <i className="fas fa-layer-group" />
+                  </h1>
+                  <h5 className="card-title">Flashcards</h5>
+                  <p className="card-text text-left">
+                    Etiam sapien lectus, pulvinar fringilla lorem ut, imperdiet
+                    vehicula ipsum. Curabitur sagittis dolor augue, at tempus
+                    massa accumsan a. Nulla sed velit at orci interdum mollis.
+                    Fusce faucibus sapien leo, in pellentesque purus molestie
+                    ultricies. Suspendisse a sem ac libero facilisis tempus.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card animated zoomIn">
+                <div className="card-body feature-cards">
+                  <h1>
+                    <i class="fas fa-feather" />
+                  </h1>
+                  <h5 className="card-title">Notes</h5>
+                  <p className="card-text text-left">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+                    in sapien congue, varius turpis at, tincidunt odio. Maecenas
+                    tincidunt magna et volutpat fringilla. Integer pretium
+                    cursus dignissim. Fusce lectus est, vestibulum ut metus et,
+                    condimentum scelerisque nisl.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card animated slideInRight">
+                <div className="card-body feature-cards">
+                  <h1>
+                    <i class="fas fa-list" />
+                  </h1>
+                  <h5 className="card-title">Lists</h5>
+                  <p className="card-text text-left">
+                    Morbi accumsan sapien vitae sodales porta. Nulla non nibh
+                    eget urna dictum hendrerit. Vivamus mattis velit sed lacus
+                    sodales, vel facilisis lectus auctor. Donec porttitor
+                    pulvinar libero, et mollis eros congue quis. Vestibulum
+                    risus ex, mattis et aliquet quis, viverra eu sem. Curabitur
+                    varius tempor mi at egestas.
+                  </p>
+                </div>
+              </div>
+              <br />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <br />
+              <div className="card animated slideInLeft">
+                <div className="card-body feature-cards">
+                  <h1>
+                    <i class="far fa-calendar-alt" />
+                  </h1>
+                  <h5 className="card-title">Reminders</h5>
+                  <p className="card-text text-left">
+                    Aliquam sed tempor justo. Praesent sit amet semper justo. In
+                    lobortis ullamcorper tellus varius vulputate. Nulla placerat
+                    vestibulum tortor, ut efficitur magna elementum eu. Nam
+                    lectus eros, elementum at nulla id, ullamcorper feugiat
+                    neque. Nullam eget sem porttitor nulla viverra cursus.
+                  </p>
+                </div>
+              </div>
+              <br />
+            </div>
+            <div className="col">
+              <br />
+              <div className="card animated slideInUp">
+                <div className="card-body feature-cards">
+                  <h1>
+                    <i class="fas fa-stopwatch" />
+                  </h1>
+                  <h5 className="card-title">Pomodoro Timer</h5>
+                  <p className="card-text text-left">
+                    Mauris in leo volutpat, sagittis ex sed, suscipit augue.
+                    Suspendisse massa ante, venenatis nec luctus at, vestibulum
+                    in felis. Nulla ante diam, pellentesque a nunc nec, varius
+                    pharetra nunc. Class aptent taciti sociosqu ad litora
+                    torquent per conubia nostra, per inceptos himenaeos.
+                    Suspendisse sollicitudin a orci ac mollis. Curabitur
+                    vehicula tristique porttitor.
+                  </p>
+                </div>
+              </div>
+              <br />
+            </div>
+
+            <div className="col">
+              <br />
+              <div className="card animated slideInRight">
+                <div className="card-body feature-cards">
+                  <h1>
+                    <i class="far fa-comment" />
+                  </h1>
+                  <h5 className="card-title">SMS</h5>
+                  <p className="card-text text-left">
+                    Donec id hendrerit eros, sed mollis sem. Etiam felis justo,
+                    volutpat tempor lacus quis, placerat interdum tortor.
+                    Aliquam in dolor vel nunc hendrerit auctor. Maecenas non
+                    ante consectetur, consequat nisi et, euismod risus. Nam id
+                    commodo lorem. Cras volutpat nisl sit amet sem laoreet
+                    posuere.
+                  </p>
+                </div>
+              </div>
+              <br />
+            </div>
+          </div>
         </div>
       </div>
-      <h1 className="text-center display-3">Features</h1>
-      <div className="container text-center">
-        <div className="row">
-          <div className="col">
-            <div className="card animated slideInLeft">
-              <div className="card-body feature-cards">
-                <h1>
-                  <i className="fas fa-layer-group" />
-                </h1>
-                <h5 className="card-title">Flashcards</h5>
-                <p className="card-text text-left">
-                  Etiam sapien lectus, pulvinar fringilla lorem ut, imperdiet
-                  vehicula ipsum. Curabitur sagittis dolor augue, at tempus
-                  massa accumsan a. Nulla sed velit at orci interdum mollis.
-                  Fusce faucibus sapien leo, in pellentesque purus molestie
-                  ultricies. Suspendisse a sem ac libero facilisis tempus.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="card animated zoomIn">
-              <div className="card-body feature-cards">
-                <h1>
-                  <i class="fas fa-feather" />
-                </h1>
-                <h5 className="card-title">Notes</h5>
-                <p className="card-text text-left">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-                  in sapien congue, varius turpis at, tincidunt odio. Maecenas
-                  tincidunt magna et volutpat fringilla. Integer pretium cursus
-                  dignissim. Fusce lectus est, vestibulum ut metus et,
-                  condimentum scelerisque nisl.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="card animated slideInRight">
-              <div className="card-body feature-cards">
-                <h1>
-                  <i class="fas fa-list" />
-                </h1>
-                <h5 className="card-title">Lists</h5>
-                <p className="card-text text-left">
-                  Morbi accumsan sapien vitae sodales porta. Nulla non nibh eget
-                  urna dictum hendrerit. Vivamus mattis velit sed lacus sodales,
-                  vel facilisis lectus auctor. Donec porttitor pulvinar libero,
-                  et mollis eros congue quis. Vestibulum risus ex, mattis et
-                  aliquet quis, viverra eu sem. Curabitur varius tempor mi at
-                  egestas.
-                </p>
-              </div>
-            </div>
-            <br />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col">
-            <br />
-            <div className="card animated slideInLeft">
-              <div className="card-body feature-cards">
-                <h1>
-                  <i class="far fa-calendar-alt" />
-                </h1>
-                <h5 className="card-title">Reminders</h5>
-                <p className="card-text text-left">
-                  Aliquam sed tempor justo. Praesent sit amet semper justo. In
-                  lobortis ullamcorper tellus varius vulputate. Nulla placerat
-                  vestibulum tortor, ut efficitur magna elementum eu. Nam lectus
-                  eros, elementum at nulla id, ullamcorper feugiat neque. Nullam
-                  eget sem porttitor nulla viverra cursus.
-                </p>
-              </div>
-            </div>
-            <br />
-          </div>
-          <div className="col">
-            <br />
-            <div className="card animated slideInUp">
-              <div className="card-body feature-cards">
-                <h1>
-                  <i class="fas fa-stopwatch" />
-                </h1>
-                <h5 className="card-title">Pomodoro Timer</h5>
-                <p className="card-text text-left">
-                  Mauris in leo volutpat, sagittis ex sed, suscipit augue.
-                  Suspendisse massa ante, venenatis nec luctus at, vestibulum in
-                  felis. Nulla ante diam, pellentesque a nunc nec, varius
-                  pharetra nunc. Class aptent taciti sociosqu ad litora torquent
-                  per conubia nostra, per inceptos himenaeos. Suspendisse
-                  sollicitudin a orci ac mollis. Curabitur vehicula tristique
-                  porttitor.
-                </p>
-              </div>
-            </div>
-            <br />
-          </div>
-
-          <div className="col">
-            <br />
-            <div className="card animated slideInRight">
-              <div className="card-body feature-cards">
-                <h1>
-                  <i class="far fa-comment" />
-                </h1>
-                <h5 className="card-title">SMS</h5>
-                <p className="card-text text-left">
-                  Donec id hendrerit eros, sed mollis sem. Etiam felis justo,
-                  volutpat tempor lacus quis, placerat interdum tortor. Aliquam
-                  in dolor vel nunc hendrerit auctor. Maecenas non ante
-                  consectetur, consequat nisi et, euismod risus. Nam id commodo
-                  lorem. Cras volutpat nisl sit amet sem laoreet posuere.
-                </p>
-              </div>
-            </div>
-            <br />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Home;
