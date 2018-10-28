45acd07e8dc31144fcf52d3fe18d071c34ef3894
Kevin Do <do.kevin.vo@gmail.com>
2018-10-28 14:15:48 -0700

    Adds attempt at conditional rendering & css

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,19 +1,40 @@
 import React from "react";
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 
+// CSS
+import "../css/Login.css";
+
 class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      modal: false
+      modal: false,
+      login: true,
+      create: false
     };
 
     this.toggle = this.toggle.bind(this);
+    this.selectLogin = this.selectLogin.bind(this);
+    this.selectCreate = this.selectCreate.bind(this);
   }
 
   toggle() {
     this.setState({
       modal: !this.state.modal
+    });
+  }
+
+  selectLogin() {
+    this.setState({
+      login: true,
+      create: false
+    });
+  }
+
+  selectCreate() {
+    this.setState({
+      login: false,
+      create: true
     });
   }
 
@@ -29,8 +50,31 @@ class Login extends React.Component {
           toggle={this.toggle}
           className={this.props.className}
         >
-          <ModalHeader toggle={this.toggle}>Login</ModalHeader>
-          <ModalBody>
+          <ModalHeader toggle={this.toggle}></ModalHeader>
+          <div className="row">
+            <div className="col">
+              <Button 
+                color="light"
+                className="login-or-create-btn" 
+                size="lg" 
+                block 
+                onClick={this.selectLogin}
+                active>
+                Sign In
+              </Button>
+            </div>
+            <div className="col">
+              <Button 
+                color="light"
+                className="login-or-create-btn"  
+                size="lg" 
+                block 
+                onClick={this.selectCreate}>
+                Sign Up
+              </Button>
+            </div>
+          </div>
+          <ModalBody selectLogin={this.selectLogin}>
             <form>
               <div class="form-group">
                 <label for="username">Username</label>
@@ -38,14 +82,18 @@ class Login extends React.Component {
                   type="text"
                   class="form-control"
                   id="username"
-                  placeholder="mysteryperson123"
+                  placeholder="alex-doe1234"
                 />
                 <label for="password">Password</label>
                 <input type="password" class="form-control" id="password" />
+                <br />
+                <Button color="primary" onClick={this.toggle}>
+                  Login
+                </Button>
               </div>
             </form>
           </ModalBody>
-          <ModalBody>
+          <ModalBody selectCreate={this.selectCreate}>
             <form>
               <div class="form-group">
                 <label for="name">Name</label>
@@ -60,19 +108,20 @@ class Login extends React.Component {
                   type="text"
                   class="form-control"
                   id="username"
-                  placeholder="mysteryperson123"
+                  placeholder="alex-doe1234"
                 />
                 <label for="password">Password</label>
                 <input type="password" class="form-control" id="password" />
                 <label for="password">Re-enter Password</label>
                 <input type="password" class="form-control" id="password" />
+                <br />
+                <Button color="primary" onClick={this.toggle}>
+                  Create
+                </Button>
               </div>
             </form>
           </ModalBody>
           <ModalFooter>
-            <Button color="primary" onClick={this.toggle}>
-              Sign In
-            </Button>{" "}
             <Button color="secondary" onClick={this.toggle}>
               Close
             </Button>
