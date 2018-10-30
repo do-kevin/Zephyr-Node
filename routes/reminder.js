b6e0be733f9c087ad73acebbefbffc1a51c71538
Chris Armendarez <chris.michael.armo@gmail.com>
2018-10-29 22:56:41 -0700

    Add more routes to reminders + todos routers

--- routes/reminder.js
+++ routes/reminder.js
@@ -9,6 +9,7 @@ const db = require("../models");
 // Export Reminder Router ---------------------------------------- /
 
 module.exports = (router) => {
+    // Retrieve all reminders on user id
     router.get("/reminders/:id", (req, res) => {
         db.Reminder
             .findAll({
@@ -18,6 +19,55 @@ module.exports = (router) => {
             })
             .then((reminders) => {
                 res.status(200).json(reminders);
+            })
+            .catch((err) => {
+                res.status(404).json(err);
+            });
+    });
+    // Retrieve one reminder on reminder id
+    router.get("/reminders/:id", (req, res) => {
+        db.Reminder
+            .findOne({
+                where: {
+                    id: req.body.id
+                }
+            })
+            .then((reminder) => {
+                res.status(200).json(reminder);
+            })
+            .catch((err) => {
+                res.status(404).json(err);
+            });
+    });
+    // Update reminder on reminder id
+    router.put("/reminders", (req, res) => {
+        const {item, date, id} = req.body;
+        db.Reminder
+            .update({
+                item,
+                date
+            }, {
+                where: {
+                    id
+                }
+            })
+            .then((reminder) => {
+                res.status(200).json(reminder);
+            })
+            .catch((err) => {
+                res.status(404).json(err);
+            });
+    });
+    // Delete reminder on reminder id
+    router.delete("/reminders", (req, res) => {
+        db.Reminder
+            .destroy({
+                where: {
+                    id: req.body.id
+                }
+            })
+            .then((reminder) => {
+                res.status(200).json(reminder);
             })
             .catch((err) => {
                 res.status(404).json(err);
--- routes/todo.js
+++ routes/todo.js
@@ -9,16 +9,66 @@ const db = require("../models");
 // Export Todo Router ---------------------------------------- /
 
 module.exports = (router) => {
-    // Get all todos by user id
-    router.get("/todos/:id", (req, res) => {
+    // Retrieve all todos on user id
+    router.get("/todos/users/:userId", (req, res) => {
         db.ToDo
             .findAll({
                 where: {
-                    id: req.params.id
+                    UserId: req.params.userId
                 }
             })
             .then((todos) => {
                 res.status(200).json(todos);
+            })
+            .catch((err) => {
+                res.status(404).json(err);
+            });
+    });
+    // Retrieve one todo on todo id
+    router.get("/todos/:id", (req, res) => {
+        db.ToDo
+            .findOne({
+                where: {
+                    id: req.params.id
+                }
+            })
+            .then((todo) => {
+                res.status(200).json(todo);
+            })
+            .catch((err) => {
+                res.status(404).json(err);
+            });
+    });
+    // Update todo on todo id
+    router.put("/todos", (req, res) => {
+        const {item, date, done, id} = req.body;
+        db.ToDo
+            .update({
+                item,
+                date,
+                done
+            }, {
+                where: {
+                    id
+                }
+            })
+            .then((todo) => {
+                res.status(200).json(todo);
+            })
+            .catch((err) => {
+                res.status(404).json(err);
+            });
+    });
+    // Delete todo on todo id
+    router.delete("/todos", (req, res) => {
+        db.ToDo
+            .destroy({
+                where: {
+                    id: req.body.id
+                }
+            })
+            .then((todo) => {
+                res.status(200).json(todo);
             })
             .catch((err) => {
                 res.status(404).json(err);
