95f922545dcd5f2ee55a5ca234f962413fc0d90b
Ana <aparakaki17@gmail.com>
2018-10-30 14:05:45 -0700

    complete twilio notifications

--- models/appointment.js
+++ models/appointment.js
@@ -1,32 +1,38 @@
 module.exports = function (sequelize, DataTypes) {
     var Appointment = sequelize.define("Appointment", {
-        name: {
+        // name: {
+        //     type: DataTypes.STRING(30),
+        //     allowNull: false,
+        //     validate: {
+        //         len: [1]
+        //     }
+        // },
+        // phoneNumber: {
+        //     type: DataTypes.STRING(15),
+        //     allowNull: false,
+        // },
+        date: {                     //actual day and time of event
             type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
+            allowNull: false
         },
-        phoneNumber: {
+        notification: {            //# of mins ahead of date that notification should be sent
             type: DataTypes.INTEGER,
-            allowNull: false,
+            allowNull: false
         },
-        notification: {
-            type: DataTypes.STRING,
-            defaultValue: false
-        },
-        timeZone: {
+        message: {                     //true if reminder has been sent already
             type: DataTypes.STRING,
             allowNull: false
         },
-        time: {
-            type: DataTypes.DATE,
-            allowNull: false
-        }
     },
         {
             timestamps: false
         });
+
+        Appointment.associate = function (models) {
+            Appointment.belongsTo(models.User, {
+                foreignKey: "userId",
+            });
+        };
 
     return Appointment;
 }
--- models/user.js
+++ models/user.js
@@ -31,15 +31,23 @@ module.exports = function (sequelize, DataTypes) {
 
         User.associate = function (models) {
             User.hasMany(models.Deck, {
+                foreignKey: "userId",
                 onDelete: 'cascade' 
             });
             User.hasMany(models.Reminder, {
+                foreignKey: "userId",
                 onDelete: 'cascade' 
             });
             User.hasMany(models.ToDo, {
+                foreignKey: "userId",
                 onDelete: 'cascade' 
             });
             User.hasMany(models.Note, {
+                foreignKey: "userId",
+                onDelete: 'cascade' 
+            });
+            User.hasMany(models.Appointment, {
+                foreignKey: "userId",
                 onDelete: 'cascade' 
             });
     };
