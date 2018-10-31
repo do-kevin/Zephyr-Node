c8fda2400aaa133f4bbce1afba278c42088d8db4
Kevin Do <do.kevin.vo@gmail.com>
2018-10-31 16:58:39 -0700

    Adds wireframe for editing decks and flipping cards

--- client/src/pages/PlayCards.js
+++ client/src/pages/PlayCards.js
@@ -12,7 +12,12 @@ import {
   Card,
   CardBody,
   CardTitle,
-  CardText
+  CardText,
+  CardSubtitle,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Form, FormGroup, Label, Input, FormText
 } from "reactstrap";
 
 // Components
@@ -25,11 +30,13 @@ class PlayCards extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      edit: false
+      edit: false,
+      openSettings: false
     };
 
     // this.refs.cardList;
     this.toggle = this.toggle.bind(this);
+    this.toggleSettings = this.toggleSettings.bind(this);
   }
 
   componentDidMount() {}
@@ -40,7 +47,14 @@ class PlayCards extends React.Component {
     });
   }
 
+  toggleSettings() {
+    this.setState({
+      openSettings: !this.state.openSettings
+    });
+  }
+
   render() {
+    //============== Card Stack==============//
     const edit = this.state.edit;
     let showCardStack;
 
@@ -49,26 +63,92 @@ class PlayCards extends React.Component {
         <div className="animated slideInUp" id="flashcard-grid" isOpen="">
           <Row>
             <Col>
-              <Card className="flashcard">
+              <Card className="flashcard animated flipInX">
+              <Button
+                    color="danger"
+                    type="button"
+                    style={{
+                      margin: "20px 10% 0 70%",
+                      borderRadius: "25px"
+                    }}
+                  >
+                    <i className="fas fa-trash-alt"></i>
+                  </Button>
                 <CardBody>
-                  <CardTitle>Question 1</CardTitle>
-                  <CardText>Answer</CardText>
+                  <CardTitle>Example Question (Front)</CardTitle>
+                  <CardText>Example Answer (Back) </CardText>
+                  <hr />
+                  <CardSubtitle>#tags</CardSubtitle>
+                  <hr />
+                  <Button
+                    type="button"
+                    style={{
+                      margin: "20px 41% 0 41%",
+                      borderRadius: "25px"
+                    }}
+                  >
+                    <i className="fas fa-save"></i>
+                  </Button>
                 </CardBody>
               </Card>
             </Col>
             <Col>
-              <Card className="flashcard">
+              <Card className="flashcard animated flipInX">
+              <Button
+                    color="danger"
+                    type="button"
+                    style={{
+                      margin: "20px 10% 0 70%",
+                      borderRadius: "25px"
+                    }}
+                  >
+                    <i className="fas fa-trash-alt"></i>
+                  </Button>
                 <CardBody>
-                  <CardTitle>Question 2</CardTitle>
-                  <CardText>Answer</CardText>
+                  <CardTitle>Example Question (Front)</CardTitle>
+                  <CardText>Example Answer (Back) </CardText>
+                  <hr />
+                  <CardSubtitle>#tags</CardSubtitle>
+                  <hr />
+                  <Button
+                    type="button"
+                    style={{
+                      margin: "20px 41% 0 41%",
+                      borderRadius: "25px"
+                    }}
+                  >
+                    <i className="fas fa-save"></i>
+                  </Button>
                 </CardBody>
               </Card>
             </Col>
             <Col>
-              <Card className="flashcard">
+              <Card className="flashcard animated flipInX">
+              <Button
+                    color="danger"
+                    type="button"
+                    style={{
+                      margin: "20px 10% 0 70%",
+                      borderRadius: "25px"
+                    }}
+                  >
+                    <i className="fas fa-trash-alt"></i>
+                  </Button>
                 <CardBody>
-                  <CardTitle>Question 3</CardTitle>
-                  <CardText>Answer</CardText>
+                  <CardTitle>Example Question (Front)</CardTitle>
+                  <CardText>Example Answer (Back) </CardText>
+                  <hr />
+                  <CardSubtitle>#tags</CardSubtitle>
+                  <hr />
+                  <Button
+                    type="button"
+                    style={{
+                      margin: "20px 41% 0 41%",
+                      borderRadius: "25px"
+                    }}
+                  >
+                    <i className="fas fa-save"></i>
+                  </Button>
                 </CardBody>
               </Card>
             </Col>
@@ -77,24 +157,39 @@ class PlayCards extends React.Component {
             <Col>
               <Card className="flashcard">
                 <CardBody>
-                  <CardTitle>Question 5</CardTitle>
-                  <CardText>Answer</CardText>
-                </CardBody>
-              </Card>
-            </Col>
-            <Col>
-              <Card className="flashcard">
-                <CardBody>
-                  <CardTitle>Question 6</CardTitle>
-                  <CardText>Answer</CardText>
-                </CardBody>
-              </Card>
-            </Col>
-            <Col>
-              <Card className="flashcard">
-                <CardBody>
-                  <CardTitle>Question 7</CardTitle>
-                  <CardText>Answer</CardText>
+                  <CardTitle>
+                    <label htmlFor="front">Question (front)</label>
+                    <Input 
+                      type="textarea" 
+                      name="text" 
+                      id="front" />
+                  </CardTitle>
+                  <CardText>
+                    <label htmlFor="back">Answer (back)</label>
+                    <Input 
+                      type="textarea" 
+                      name="text" 
+                      id="back" />
+                  </CardText>
+                  <hr />
+                  <CardSubtitle>
+                    <label htmlFor="tags">Tags</label>
+                    <input 
+                      type="text" 
+                      className="form-control" 
+                      id="tags" />
+                  </CardSubtitle>
+                  <br />
+                  <Button
+                    type="submit"
+                    style={{
+                      marginLeft: "41%",
+                      marginRight: "41%",
+                      borderRadius: "25px"
+                    }}
+                  >
+                    <i className="fas fa-plus" />
+                  </Button>
                 </CardBody>
               </Card>
             </Col>
@@ -104,6 +199,88 @@ class PlayCards extends React.Component {
     } else if (edit === false) {
       showCardStack = (
         <div className="animated fadeOut" id="flashcard-grid" isOpen="" />
+      );
+    }
+
+    //============== Deck Settings ==============//
+
+    const openSettings = this.state.openSettings;
+    let modal;
+
+    if (openSettings === true) {
+      modal = (
+        <div>
+          <Modal
+            isOpen={this.state.openSettings}
+            toggle={this.openSettings}
+            className={this.props.className}
+          >
+            <ModalHeader toggle={this.openSettings}>
+              <Row>
+                <Col>
+                  <h1 className="text-center">Settings</h1>
+                </Col>
+                <Col style={{ right: "-250px" }}>
+                  <Button onClick={this.toggleSettings}>
+                    <i className="fas fa-times" />
+                  </Button>
+                </Col>
+              </Row>
+            </ModalHeader>
+            <ModalBody>
+              <Row style={{ margin: "auto" }}>
+                <Col>
+                  <p>Make deck public</p>
+                </Col>
+                <Col>
+                  <label className="switch">
+                    <input type="checkbox" />
+                    <span className="toggle-slider round" />
+                  </label>
+                </Col>
+              </Row>
+              <Row style={{ margin: "auto" }}>
+                <Col>
+                  <p>Send text message</p>
+                </Col>
+                <Col>
+                  <label className="switch">
+                    <input type="checkbox" />
+                    <span className="toggle-slider round" />
+                  </label>
+                </Col>
+              </Row>
+              <Row style={{ margin: "auto" }}>
+                <Col>
+                  <p style={{ textAlign: "left" }}>Send questions every</p>
+                </Col>
+                <select>
+                  <option value="10">10</option>
+                  <option value="20">20</option>
+                  <option value="30">30</option>
+                  <option value="45">45</option>
+                </select>
+                <Col>
+                  <p>minutes</p>
+                </Col>
+              </Row>
+              <Row style={{ margin: "auto" }}>
+                <Col>
+                  <p style={{ textAlign: "left" }}>Send the answers every</p>
+                </Col>
+                <select>
+                  <option value="5">5</option>
+                  <option value="10">10</option>
+                  <option value="15">15</option>
+                  <option value="30">30</option>
+                </select>
+                <Col>
+                  <p>minutes.</p>
+                </Col>
+              </Row>
+            </ModalBody>
+          </Modal>
+        </div>
       );
     }
 
@@ -182,11 +359,12 @@ class PlayCards extends React.Component {
           <Button color="primary" id="edit-deck-btn" onClick={this.toggle}>
             Edit
           </Button>
-          <Button id="settings-btn" color="dark">
+          <Button id="settings-btn" color="dark" onClick={this.toggleSettings}>
             <i class="fas fa-cogs" />
           </Button>
         </div>
         {showCardStack}
+        {modal}
       </div>
     );
   }
