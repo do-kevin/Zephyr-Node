150f9deeda6c797bec069c55d469def9ff8ccb96
Kevin Do <do.kevin.vo@gmail.com>
2018-11-04 03:09:28 -0800

    Manually adds missing styling for a few toolbar options

--- client/src/pages/Notes.js
+++ client/src/pages/Notes.js
@@ -1,9 +1,10 @@
 import React from "react";
-import ReactQuill from "react-quill";
+import ReactQuill, { Quill } from "react-quill";
 import axios from "axios";
 
 // Components
 import Sidebar from "../components/Sidebar";
+
 
 // CSS
 import "../css/Notes.css";
@@ -12,50 +13,78 @@ class Note extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      text: "",
       note: "",
       userId: JSON.parse(localStorage.getItem("user")).id
     };
     this.handleChange = this.handleChange.bind(this);
+    this.saveNote = this.saveNote.bind(this);
   }
+  
 
   modules = {
     toolbar: [
-      [{ 'header': [1, 2, false] }],
-      ['bold', 'italic', 'underline','strike', 'blockquote'],
-      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-      ['link', 'image'],
-      ['clean']
+      [{ header: [1, 2, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" }
+      ],
+      ["link", "image", "video", "code-block"],
     ],
-  }
+    keyboard:   { 
+        bindings:   [
+            {tab: false}
+        ]
+    }
+  };
 
   formats = [
-    'header',
-    'bold', 'italic', 'underline', 'strike', 'blockquote',
-    'list', 'bullet', 'indent',
-    'link', 'image'
-  ]
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+    "video",
+    "code-block"
+  ];
 
   handleChange(value) {
-    this.setState({ note: value });
+    this.setState({ text: value });
   }
 
-  componentDidMount() {
+  saveNote = event => {
+    event.preventDefault();
 
+    this.setState({ note: this.state.text });
+  };
+
+  componentDidMount() {
   }
 
   render() {
-    if (this.state.note !== null) {
-        console.log(this.state.note)
-    }
-
     return (
       <div>
         <Sidebar />
         <h1>Notes</h1>
-        <ReactQuill value={this.state.note} onChange={this.handleChange} modules={this.modules}
-                    formats={this.formats}/>
-
-        <div dangerouslySetInnerHTML={{__html: this.state.note}}></div>
+        <p>Create an <strong>embed link</strong>: Type it, highlight it, and then click on the link button</p>
+        <ReactQuill
+          theme="snow"
+          value={this.state.text}
+          onChange={this.handleChange}
+          modules={this.modules}
+          formats={this.formats}
+        />
+        <button onClick={this.saveNote}>Show Note on Click</button>
+        {<div dangerouslySetInnerHTML={{ __html: this.state.note }} style={{ wordBreak: "break-word"}}/>}
       </div>
     );
   }
