413793a0329b9c0bad046b5a66f63708ca5a808c
Kevin Do <do.kevin.vo@gmail.com>
2019-01-13 04:25:41 -0800

    Replaces with process.env

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 var keys = {
-    accountSid: 'ACab9d13f032b13f7e956bb20b0a9b4202',
-    authToken: 'af3da6a2e4f3c4b6c422d25570bba03f'
+    accountSid: process.env.ACCOUNT_ID,
+    authToken: process.env.AUTH_TOKEN
 }
 
 module.exports = keys;
