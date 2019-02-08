d46ac8287575a572b57fe1220cce0d30330dc6c3
Kevin Do <do.kevin.vo@gmail.com>
2019-02-07 20:47:18 -0800

    Cleans up CSS and applies BEM

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -2,19 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 // CSS
-import "../css/Sidebar.css";
+import "../css/Sidebar/Sidebar.css";
 
 class Sidebar extends React.Component {
   constructor(props) {
     super(props);
 
-    this.toggle = this.toggle.bind(this);
     this.state = {
       isOpen: true
     };
   };
 
-  toggle() {
+  toggle = () => {
     this.setState({
       isOpen: !this.state.isOpen
     });
@@ -25,50 +24,46 @@ class Sidebar extends React.Component {
 
     if (this.state.isOpen) {
       showMenu = (
-        <div id="wrapper" className="toggled">
-          <div id="sidebar-wrapper">
+        <div id="wrapper">
+          <div id="sidebar">
             <ul className="sidebar-nav">
-              {/* <li className="sidebar-brand sidebar-nav-item">
-                <Link to="/">Home</Link>
-              </li> */}
               <li 
-                className="sidebar-brand sidebar-nav-item"
+                className="sidebar-nav__brand sidebar-nav__link sidebar-nav__link--profile"
                 id="profile-span"
               >
                 <Link to="/profile">
                 <span 
-                  className="sidebar-nav-span"
+                  className="sidebar-nav__text"
                   style={{fontSize: "20px"}}
                   >Profile</span></Link>
               </li>
-              <li className="sidebar-nav-item">
-                <Link to="/choose"><span className="sidebar-nav-span">Decks</span></Link>
+              <li className="sidebar-nav__link sidebar-nav__link--decks">
+                <Link to="/decks"><span className="sidebar-nav__text">Decks</span></Link>
               </li>
-              <li className="sidebar-nav-item">
-                <Link to="/notes"><span className="sidebar-nav-span">Notes</span></Link>
+              <li className="sidebar-nav__link sidebar-nav__link--notes">
+                <Link to="/notes"><span className="sidebar-nav__text">Notes</span></Link>
               </li>
-              <li className="sidebar-nav-item">
-                <Link to="/reminder"><span className="sidebar-nav-span">Reminders</span></Link>
+              <li className="sidebar-nav__link sidebar-nav__link--reminders">
+                <Link to="/reminder"><span className="sidebar-nav__text">Reminders</span></Link>
               </li>
-              <li className="sidebar-nav-item">
-                <Link to="/todo"><span className="sidebar-nav-span">To-Do</span></Link>
+              <li className="sidebar-nav__link sidebar-nav__link--todo">
+                <Link to="/todo"><span className="sidebar-nav__text">To-Do</span></Link>
               </li>
               <li
                 style={{marginTop: "30%"}}
-                className="sidebar-nav-item"
+                className="sidebar-nav__link sidebar-nav__link--settings"
                 id="settings-span">
                 <Link to="/settings">
                 <span 
-                  className="sidebar-nav-span"
+                  className="sidebar-nav__text"
                   style={{fontSize: "18px"}}
                 >Settings</span></Link>
               </li>
               <li>
                 <button 
-                  className="btn log-out-btn"
+                  className="log-out-btn"
                   type="button"
                   onClick={this.props.handleUserLogout}
-                  style={{marginLeft: "22%", marginTop: "40%"}}
                 >
                   <i className="fas fa-sign-out-alt"></i>{" "}Log Out 
                 </button>
@@ -85,7 +80,7 @@ class Sidebar extends React.Component {
     return (
       <div>
         {showMenu}
-        <div id="page-content-wrapper">
+        <div id="wrapper__content">
           <div className="container-fluid">
             {/* Where we would insert our pages */}
 
