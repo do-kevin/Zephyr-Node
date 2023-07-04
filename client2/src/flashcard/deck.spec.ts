6263bf237993fc4e8f995f390f41de66dedd3394
Kevin Do <do.kevin.vo@gmail.com>
2023-07-04 01:26:28 -0700

    :white_check_mark: sets up test for loading decks

--- client2/src/flashcard/deck.spec.ts
+++ client2/src/flashcard/deck.spec.ts
@@ -1,4 +1,28 @@
+import { TestHarness } from "src/tests/tools/test-harness";
+import { DeckPresenter } from "./DeckPresenter";
+import { DeckRepository } from "./DeckRepository";
+
+let testHarness = null;
+let deckPresenter: InstanceType<typeof DeckPresenter>;
+let deckRepository: InstanceType<typeof DeckRepository>;
+
+beforeEach(async () => {
+  testHarness = new TestHarness();
+  testHarness.init();
+
+  deckPresenter = testHarness.container.get(DeckPresenter);
+  deckRepository = testHarness.container.get(DeckRepository);
+});
+
+afterEach(() => jest.restoreAllMocks());
+
 it("Should list public decks", async () => {
-  await presenter.list();
-  expect(presenter.decks.length).toBe(3);
+  await deckRepository.list();
+
+  const decks = deckPresenter.decks;
+
+  expect(decks.length).toBe(3);
+  expect(decks[0].subject).toBe("Stubbing");
+  expect(decks[1].subject).toBe("Mocking");
+  expect(decks[decks.length - 1].subject).toBe("React");
 });
