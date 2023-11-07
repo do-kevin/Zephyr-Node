746fa74932d2ab9c55090d74349db93e5d288b9a
Kevin Do <do.kevin.vo@gmail.com>
2023-11-06 22:43:14 -0800

    :construction: :lipstick: Flashcard Search shows a result list pop up

--- client2/src/flashcard/DeckList.tsx
+++ client2/src/flashcard/DeckList.tsx
@@ -9,7 +9,7 @@ export const DeckList = (props: Props) => {
   return (
     <div className="d-flex flex-wrap gap-3">
       {props.viewModel?.map((d) => {
-        console.log("d: ", d);
+        // console.log("d: ", d);
         return (
           <article key={d.id} className="w-100 mx-50">
             <div className="bg-white d-inline-block p-5 shadow-sm rounded w-100">
--- client2/src/flashcard/DiscoverFlashcardPage.tsx
+++ client2/src/flashcard/DiscoverFlashcardPage.tsx
@@ -1,18 +1,56 @@
-import { useEffect } from "react";
-import * as presenter from "./DeckPresenter";
-import { DeckList } from "./DeckList";
-import { useAppDispatch, useAppSelector } from "./DeckRepository";
-import Form from "react-bootstrap/Form";
+import { matchSorter } from "match-sorter";
+import { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
+import { useForm } from "react-hook-form";
+
+import { DeckList } from "./DeckList";
+import * as presenter from "./DeckPresenter";
+import { useAppDispatch, useAppSelector } from "./DeckRepository";
 
 export const DiscoverFlashcardPage = () => {
   const dispatch = useAppDispatch();
   const decksVM = useAppSelector(presenter.selectDecks);
 
+  const [searchValue, setSearchValue] = useState<string | null | undefined>("");
+  const [results, setResults] = useState<any[]>([]);
+
+  const onSubmit = (values: any) => {
+    // console.log("Values:::", values);
+    // console.log("Values:::", JSON.stringify(values));
+  };
+
+  const onError = (error: any) => {
+    console.log("ERROR:::", error);
+  };
+
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    watch,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      searchValue,
+    },
+  });
+
   useEffect(() => {
     dispatch<any>(presenter.loadPublicDecks());
   }, [dispatch]);
+
+  useEffect(() => {
+    const subscription = watch((value, { name, type }) => {
+      setSearchValue(value.searchValue);
+      console.log(value);
+      const result = matchSorter(decksVM, searchValue, { keys: ["subject"] });
+      setResults(result);
+    });
+
+    return () => subscription.unsubscribe();
+  }, [watch, decksVM, searchValue]);
 
   return (
     <div style={{ backgroundColor: "#fffaf5" }}>
@@ -47,10 +85,17 @@ export const DiscoverFlashcardPage = () => {
                 }
               `}
             </style>
-            <Form className="d-flex align-items-center mb-3">
+            <Form
+              className="custom-search d-flex align-items-center flex-column"
+              onSubmit={handleSubmit(onSubmit, onError)}
+            >
               <InputGroup className="mb-3">
-                <div className="bg-white d-block border border-end-0 border-dark px-2 py-2  rounded-start-5">
+                <div
+                  id="search-btn-container"
+                  className="bg-white d-block border border-end-0 border-dark px-2 py-2  rounded-start-5"
+                >
                   <Button
+                    type="submit"
                     variant="warning"
                     id="button-addon1"
                     className="rounded-5"
@@ -63,7 +108,28 @@ export const DiscoverFlashcardPage = () => {
                   id="discoverInput"
                   className="border-dark border-start-0 py-2 rounded-end-5"
                   placeholder="Find a deck"
+                  {...register("searchValue", { required: true })}
                 />
+
+                {searchValue && searchValue.length && results.length >= 1 && (
+                  <div
+                    className="position-absolute top-100 bg-white container py-3 px-2 mt-2 shadow"
+                    style={{ border: "1px solid #FA963A", borderRadius: 4 }}
+                  >
+                    <ul className="list-group mb-0">
+                      {results.map((r) => {
+                        return (
+                          <li
+                            className="list-group-item border-0 mb-2 rounded"
+                            style={{ background: "#FEF7F1", color: "#F97D0B" }}
+                          >
+                            {r.subject}
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </div>
+                )}
               </InputGroup>
             </Form>
           </>
