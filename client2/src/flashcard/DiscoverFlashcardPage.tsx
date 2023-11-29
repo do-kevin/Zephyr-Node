ff676f91fc6a0b5fa2e966f9f88d0682e3b1b6d1
Kevin Do <do.kevin.vo@gmail.com>
2023-11-28 23:02:03 -0800

    chore: can compile Bootstrap scss to css

--- client2/src/flashcard/DiscoverFlashcardPage.tsx
+++ client2/src/flashcard/DiscoverFlashcardPage.tsx
@@ -111,86 +111,96 @@ export const DiscoverFlashcardPage = () => {
   };
 
   return (
-    <div style={{ backgroundColor: "#fffaf5" }}>
+    <div className="bg-light">
       <header className="py-3 px-3 px-sm-5 border-bottom border-dark mb-4">
-        <nav className="d-flex flex-row flex-wrap align-items-center">
-          <h1 className="fs-5 fw-bold mb-0 col" style={{ color: "#FA963A" }}>
-            NoteCram
-          </h1>
-          <div className="d-flex justify-content-between align-items-center col-8">
-            <ul
-              className="list-group list-group-horizontal list-unstyled"
-              style={{ fontSize: 12 }}
-            >
-              <li className="me-2">Features</li>
-              <li className="me-2">Discover</li>
-              <li>Pricing</li>
-            </ul>
-            <p className="d-inline text-end my-auto" style={{ fontSize: 12 }}>
-              Sign in
-            </p>
-          </div>
-        </nav>
+        <div className="mx-auto" style={{ maxWidth: 1200 }}>
+          <nav className="d-flex flex-row flex-wrap align-items-center">
+            <h1 className="fs-5 fw-bold mb-0 col-2 text-primary">NoteCram</h1>
+            <div className="d-flex justify-content-between align-items-center col-8">
+              <ul
+                className="list-group list-group-horizontal list-unstyled"
+                style={{ fontSize: 12 }}
+              >
+                <li className="me-2">Features</li>
+                <li className="me-2">Discover</li>
+                <li>Pricing</li>
+              </ul>
+              <p className="d-inline text-end my-auto" style={{ fontSize: 12 }}>
+                Sign in
+              </p>
+            </div>
+          </nav>
+        </div>
       </header>
       <main className="px-3 px-sm-5 pb-4">
-        <article>
-          <h1 className="fw-bold mb-3">Discover</h1>
-          <>
-            <style type="text/css">
-              {`
+        <article className="mx-auto" style={{ maxWidth: 1200 }}>
+          <div className="d-flex align-items-end gap-4">
+            <h1
+              className="fw-bold mb-3 col position-relative"
+              style={{ fontSize: 128, right: "0.5rem" }}
+            >
+              Discover
+            </h1>
+            <>
+              <style type="text/css">
+                {`
                 #discoverInput::placeholder {
                   color: #545454;
                 }
               `}
-            </style>
-            <Form
-              className="custom-search d-flex align-items-center flex-column"
-              onSubmit={handleSubmit(onSubmit, onError)}
-            >
-              <InputGroup className="mb-3">
-                <div
-                  id="search-btn-container"
-                  className="bg-white d-block border border-end-0 border-dark px-2 py-2  rounded-start-5"
-                >
-                  <Button
-                    type="submit"
-                    variant="warning"
-                    id="button-addon1"
-                    className="rounded-5"
-                  >
-                    <i className="bi bi-search"></i>
-                  </Button>
-                </div>
-                <Form.Control
-                  aria-describedby="discoverInput"
-                  id="discoverInput"
-                  className="border-dark border-start-0 py-2 rounded-end-5"
-                  placeholder="Find a deck"
-                  {...register("searchValue", { required: true })}
-                />
-                {showMatches && (
+              </style>
+              <Form
+                className="custom-search d-flex align-items-center flex-column col"
+                onSubmit={handleSubmit(onSubmit, onError)}
+              >
+                <InputGroup className="mb-3">
                   <div
-                    className="position-absolute top-100 bg-white container py-3 px-2 mt-2 shadow"
-                    style={{ border: "1px solid #FA963A", borderRadius: 4 }}
+                    id="search-btn-container"
+                    className="bg-white d-block border border-end-0 border-dark px-2 py-2  rounded-start-5"
                   >
-                    <ul className="list-group mb-0">
-                      {resultViewModel.map((r) => {
-                        return (
-                          <li
-                            className="list-group-item border-0 mb-2 rounded"
-                            style={{ background: "#FEF7F1", color: "#F97D0B" }}
-                            onClick={() => resultsOnPress()}
-                          >
-                            {r.subject}
-                          </li>
-                        );
-                      })}
-                    </ul>
+                    <Button
+                      type="submit"
+                      variant="warning"
+                      id="button-addon1"
+                      className="rounded-5"
+                    >
+                      <i className="bi bi-search"></i>
+                    </Button>
                   </div>
-                )}
-              </InputGroup>
-            </Form>
-          </>
+                  <Form.Control
+                    aria-describedby="discoverInput"
+                    id="discoverInput"
+                    className="border-dark border-start-0 py-2 rounded-end-5"
+                    placeholder="Find a deck"
+                    {...register("searchValue", { required: true })}
+                  />
+                  {showMatches && (
+                    <div
+                      className="position-absolute top-100 bg-white container py-3 px-2 mt-2 shadow"
+                      style={{ border: "1px solid #FA963A", borderRadius: 4 }}
+                    >
+                      <ul className="list-group mb-0">
+                        {resultViewModel.map((r) => {
+                          return (
+                            <li
+                              className="list-group-item border-0 mb-2 rounded"
+                              style={{
+                                background: "#FEF7F1",
+                                color: "#F97D0B",
+                              }}
+                              onClick={() => resultsOnPress()}
+                            >
+                              {r.subject}
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    </div>
+                  )}
+                </InputGroup>
+              </Form>
+            </>
+          </div>
           {showResults && (
             <Button
               className="text-decoration-none py-0 ps-0 pe-1 mb-1"
@@ -205,6 +215,7 @@ export const DiscoverFlashcardPage = () => {
               </span>
             </Button>
           )}
+          <h2 className="text-primary fw-bold mb-4">Flashcard Decks</h2>
           <div className="d-grid">
             <DeckList viewModel={currentItems} />
             <div className="d-flex justify-content-center mt-4 pb-4 gap-2">
@@ -233,8 +244,14 @@ export const DiscoverFlashcardPage = () => {
                 />
                 <Pagination.Last onClick={() => setCurrentPage(totalPages)} />
               </Pagination>
-
-              <DropdownButton title={itemsPerPage}>
+              <style type="text/css">
+                {`
+                #my-dropdown {
+                  color: var(--bs-secondary);
+                }
+              `}
+              </style>
+              <DropdownButton title={itemsPerPage} id="my-dropdown">
                 {[5, 10, 15, 20].map((cardsPerPage) => {
                   return (
                     <Dropdown.Item
