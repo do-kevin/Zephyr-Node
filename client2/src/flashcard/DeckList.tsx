64557e9d56c5fba23984f94bd288eff9e1100659
Kevin Do <do.kevin.vo@gmail.com>
2023-12-02 17:39:53 -0800

    :contruction: flashcard deck cover concept

--- client2/src/flashcard/DeckList.tsx
+++ client2/src/flashcard/DeckList.tsx
@@ -7,15 +7,21 @@ interface Props {
 
 export const DeckList = (props: Props) => {
   return (
-    <div className="d-flex flex-wrap gap-3">
+    <div className="d-flex flex-wrap gap-5 mb-5">
       {props.viewModel?.map((d) => {
         return (
           <article
             key={d.id}
-            className="mx-50 col-12 col-md-6 col-lg-4 w-100 w-md-45 w-lg-32"
+            className="mx-50 col-12 col-md-6 col-lg-4 w-100 w-md-45 w-lg-30 test-card-shadow"
+            style={{
+              borderRadius: 4,
+            }}
           >
-            <div className="bg-white d-inline-block p-5 shadow-sm rounded w-100 h-100">
-              <p className="text-center mb-0 py-3">{d.subject}</p>
+            <div
+              tabindex="0"
+              className="test-shadow-on-hover bg-white d-inline-block p-5 shadow-sm rounded w-100 h-100"
+            >
+              <p className="text-center mb-0 py-5">{d.subject}</p>
             </div>
           </article>
         );
